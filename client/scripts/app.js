cabc609a0b88f66fa8af4380ad4771cb393b3121
Faris Habib <faris.habib.7@gmail.com>
2018-09-18 12:16:55 -0700

    Almost done.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -34,24 +34,17 @@ app.fetch = function(message) {
     dataType: 'json',
     contentType: 'application/json',
     success: function (data) {
-      // iterate through results
-      // if index in results has username and text properties
-      //append username and text properties to chat
-      //console.log('Data: ' + JSON.stringify(data));
-      console.log(JSON.stringify(app.roomname));
       for (var i = 0; i < data.results.length; i++) {
         if (data.results[i].username && data.results[i].text && data.results[i].roomname) {
           if (!app.roomname.hasOwnProperty(data.results[i].roomname)) {
             app.roomname[data.results[i].roomname] = data.results[i].roomname;
           }
-          //console.log(JSON.stringify('rooms: ' + JSON.stringify(app.roomname[data.results[i].roomname])));
           if (app.antiHack(data.results[i].text)) {
             $('#chats').append('<div class="chat">' + '<span class="username" data-user></span></div>' + data.results[i].username + ':' + ' ' + data.results[i].text);
           }
         }
       }
       for (var key in app.roomname) {
-        console.log('rooms: ' + key);
         $('#roomSelect').append('<option>' + app.roomname[key] + '</option>');
       }
     },
@@ -105,28 +98,12 @@ $(document).ready(function() {
   };
 
   var message = {
-    username: 'Mr. Meeseeks',
-    text: '<img src="images/meeseeks.gif" height="500" width="500">',
-    roomname: 'C137'
+    username: 'A Concerned Citizen',
+    text: '༼ つ ◕_◕ ༽つ RPT10 TAKE MY ENERGY TO FINISH THIS SPRINT ༼ つ ◕_◕ ༽つ',
+    roomname: 'Lobby'
   };
 
   app.fetch();
   app.send(message);
-  //app.init = function () {};
-  // app.send = function () {};
-  // $.ajax({
-  //   // This is the url you should use to communicate with the parse API server.
-  //   url: 'http://parse.rpt.hackreactor.com/chatterbox/classes/messages',
-  //   type: 'POST',
-  //   data: JSON.stringify(message),
-  //   contentType: 'application/json',
-  //   success: function (data) {
-  //     console.log('chatterbox: Message sent');
-  //   },
-  //   error: function (data) {
-  //     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-  //     console.error('chatterbox: Failed to send message', data);
-  //   }
-  // });
 
 });
